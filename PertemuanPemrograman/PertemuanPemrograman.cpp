247aa328b29a175e71c687f3765fd939601c6022
M S I <M S I@MSI>
2024-02-26 13:21:18 +0700

    Add project files.

---
+++ PertemuanPemrograman/PertemuanPemrograman.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    //begin
+        // numerik nJejari, nLuas
+        // display 'Masukan jejari'
+        // accept nJejari
+        // compute nLuas = 3,14 * r * r
+        // display 'Luasnya' + nLuas
+    //end
+    double nJejari, nLuas;
+    
+
+    system("pause");
+}
+
